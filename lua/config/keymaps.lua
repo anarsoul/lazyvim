e9e9de89585a193cf07cf1c2f9639e4a062e572a
Vasily Khoruzhick <anarsoul@gmail.com>
2023-03-10 16:45:42 -0800

    Add some favorite keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap.set
+local opts = { silent = true }
+
+keymap("n", "=", ":bnext<CR>", opts)
+keymap("n", "-", ":bprevious<CR>", opts)
+keymap("n", "t", ":enew<CR>")
